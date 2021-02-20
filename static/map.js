8a3c7018969abe465648c3372754fffa948eac6b
Ekuyik Essien <Crimsonking.ene@gmail.com>
2021-02-20 23:56:37 +0100

    canada update

--- static/map.js
+++ static/map.js
@@ -1,41 +1,36 @@
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 function initMap() {
-    const uluru = { lat: -25.363, lng: 131.044 };
+    const canada = { lat: 56.1304, lng: -106.3468 };
+    const newfoundland = {lat: 53.1355, lng: -57.6604};
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+        zoom: 4,
+        center: canada,
     });
     const contentString =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      "(last visited June 22, 2009).</p>" +
-      "</div>" +
-      "</div>";
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">Canada</h1>' +
+        '<div id="bodyContent">' +
+        "<p><b>Canada</b>, An attempt at digitalizing Essential Healthcare Processes.</p>" +
+        "</div>" +
+        "</div>";
     const infowindow = new google.maps.InfoWindow({
-      content: contentString,
+        content: contentString,
     });
     const marker = new google.maps.Marker({
-      position: uluru,
-      map,
-      title: "Uluru (Ayers Rock)",
+        position: canada,
+        map,
+        title: "Canada",
     });
     marker.addListener("click", () => {
-      infowindow.open(map, marker);
+        infowindow.open(map, marker);   
+    const marker = new google.maps.Marker({
+        position: newfoundland,
+        map,
+        title: "Newfoundland"
     });
-  }
+
+    });
+}
